8e9e522db5fef55c5fab8fb000d6baf59be87c26
Pinaki Batabyal <pinakibatabyal@gmail.com>
2025-12-21 15:58:17 +0530

    updated the photos

--- src/components/Screen5_PhotoGallery.jsx
+++ src/components/Screen5_PhotoGallery.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const Screen5_PhotoGallery = ({ onNext, onPrev }) => {
-  const photoCount = 10;
+  const photoCount = 12;
   const photos = Array.from({ length: photoCount }, (_, i) => ({
     id: i,
     src: `/photos/photo${i + 1}.jpg`,
